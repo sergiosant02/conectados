2885a1157490f9ef5a053c6b56fc3ecc10b7d606
sergiosant02 <sergiosantiago0403@gmail.com>
2024-04-29 06:53:32 +0200

    disable cors

--- conectadosBack/src/main/java/com/sergiosantiago/conectados/config/WebConfig.java
+++ conectadosBack/src/main/java/com/sergiosantiago/conectados/config/WebConfig.java
@@ -93,10 +93,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
     @Bean
